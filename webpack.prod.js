eeae550cafc267dcbeabe7e879d459fc97362634
Eryk Słociński <eryk.slocinski@gmail.com>
2019-09-11 13:32:01 +0200

    New functionality autoprefixer with grid support

--- webpack.prod.js
+++ webpack.prod.js
@@ -36,9 +36,11 @@ module.exports = merge(common, {
       {
         test: /\.scss$/,
         use: [
-          MiniCssExtractPlugin.loader, //3. Extract css into files
-          'css-loader', //2. Turns css into commonjs
-          'sass-loader', //1. Turns sass into css
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
         ],
       },
     ],
